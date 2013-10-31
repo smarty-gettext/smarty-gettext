a91a9236b80bead03820318e7994676de1cc8e6b
Elan Ruusamäe <glen@delfi.ee>
2013-10-31 17:09:58 +0200

    tests for escaping

--- tests/TestTranslate.php
+++ tests/TestTranslate.php
@@ -23,9 +23,17 @@ class TestTranslate extends PHPUnit_Framework_TestCase {
 	}
 
 	public function testData() {
-		// $input, $output
+		// string $expected, string $input, array $params
 		return array(
 			array('tere tali', 'tere %1', array('1' => 'tali')),
+
+			// various escapes
+			array('kommivabrik &quot;kalev&quot;', 'kommivabrik "kalev"', array()),
+			array('kommivabrik &quot;kalev&quot;', 'kommivabrik "kalev"', array('escape' => 'html')),
+			array("kommivabrik \'kalev\'", "kommivabrik 'kalev'", array('escape' => 'js')),
+			array('kommivabrik "kalev"', 'kommivabrik "kalev"', array('escape' => 'js')),
+			array('kommivabrik+%22kalev%22', 'kommivabrik "kalev"', array('escape' => 'url')),
+			array("kommivabrik+%27kalev%27", "kommivabrik 'kalev'", array('escape' => 'url')),
 		);
 	}
 }
