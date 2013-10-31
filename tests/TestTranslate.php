f99073dd9746314cc4380d59766146f6473fdfe7
Elan Ruusamäe <glen@delfi.ee>
2013-10-31 17:16:42 +0200

    tests for javascript escapes.
    silly examples provided by kalle volkov :)

--- tests/TestTranslate.php
+++ tests/TestTranslate.php
@@ -31,12 +31,14 @@ class TestTranslate extends PHPUnit_Framework_TestCase {
 			array('kommivabrik &quot;kalev&quot;', 'kommivabrik "kalev"', array()),
 			array('kommivabrik &quot;kalev&quot;', 'kommivabrik "kalev"', array('escape' => 'html')),
 			array("kommivabrik \'kalev\'", "kommivabrik 'kalev'", array('escape' => 'js')),
-			array('kommivabrik "kalev"', 'kommivabrik "kalev"', array('escape' => 'js')),
+			array('kommivabrik \"kalev\"', 'kommivabrik "kalev"', array('escape' => 'js')),
 			array('kommivabrik+%22kalev%22', 'kommivabrik "kalev"', array('escape' => 'url')),
 			array("kommivabrik+%27kalev%27", "kommivabrik 'kalev'", array('escape' => 'url')),
 
 			// slashes
 			array("Check from \\\\smb\\myshare", "Check from \\\\smb\\myshare", array('escape' => 'html')),
+			array("\t\\r\\nCheck from \\\\\\\\smb\\\\myshare", "\t\r\nCheck from \\\\smb\\myshare", array('escape' => 'js')),
+			array('Type in \"\\\\server.name\\\\share\\\\\" and press enter', 'Type in "\\server.name\share\" and press enter', array('escape' => 'js')),
 		);
 	}
 }
